001fd2e3bac6e276e1fdcae9e6cd4dcbf8a4c076
Square Interview <itsupport+interview@squareup.com>
2019-08-14 08:19:24 -0700

    added in geolocation and also form to be able to add new businesses

--- payitforward/app/assets/javascripts/places.js
+++ payitforward/app/assets/javascripts/places.js
@@ -7,4 +7,35 @@ function initMap(lat, lng) {
     zoom: 14
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var infoWindow = new google.maps.InfoWindow;
+
+// Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('👋  You are here!');
+      var marker = new google.maps.Marker({position: pos, map: map});
+      infoWindow.open(map, marker);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
