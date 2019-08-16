5793c5573724993cd11a66371de26c2f1698ff4a
Square Interview <itsupport+interview@squareup.com>
2019-08-15 20:44:57 -0700

    removed extra dir

--- pay_it_forward/pay_it_forward/vendor/bundle/ruby/2.3.0/gems/capybara-3.15.1/lib/capybara/selector/xpath_extensions.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module XPath
-  class Renderer
-    def join(*expressions)
-      expressions.join('/')
-    end
-  end
-end
--- pay_it_forward/pay_it_forward/vendor/bundle/ruby/2.3.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/square/Documents/Code-Camp-Hackathon-2019/pay_it_forward/pay_it_forward/vendor/bundle/ruby/2.3.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
